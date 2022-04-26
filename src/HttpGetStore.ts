7d771cf5cf5ac75ed49acc5a97d453c9fce81c6a
Sindhu-Vasireddy <98752594+Sindhu-Vasireddy@users.noreply.github.com>
2022-04-26 09:25:35 +0100

    Update HttpGetStore.ts

--- src/HttpGetStore.ts
+++ src/HttpGetStore.ts
@@ -5,7 +5,6 @@ import {
   ResourceIdentifier,
 } from "@solid/community-server";
 import fetch from "node-fetch";
-//import { RepresentationPreferences } from "@solid/community-server/dist/ldp/representation/RepresentationPreferences";
 const parseContentType = require("content-type").parse;
 
 class resource implements ResourceIdentifier {
@@ -14,7 +13,6 @@ class resource implements ResourceIdentifier {
     this.path=path;
   }
 }
-
 
 /**
  * Fetches the resource at an URL
@@ -27,7 +25,6 @@ export class HttpGetStore extends BaseResourceStore {
   public constructor(options: { url: string[] }) {
     super();
     this.url = options.url;
-    console.log(this.url[1]);
     this.current_resourceID= new resource(options.url[0]);
     this.url.forEach(async (u)=>{this.current_resourceID.path=u; this.resource.push(await this.getRepresentation(this.current_resourceID));console.log(this.resource);});
     
@@ -39,10 +36,8 @@ export class HttpGetStore extends BaseResourceStore {
 
   public async getRepresentation(
     identifier: ResourceIdentifier
-    // preferences: RepresentationPreferences
   ): Promise<Representation> {
     const response = await fetch(identifier.path);
-    // console.log(identifier.path);
     const text = await response.text();
     let contentType = response.headers.get("content-type") || "text/plain";
     contentType = parseContentType(contentType).type;
